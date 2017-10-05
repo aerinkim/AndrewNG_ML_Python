e8f85b4b6d4c3728e4cc4b53c849edafc88edb08
byorxyz <ohmyheroine@hotmail.com>
2017-10-05 00:19:47 -0700

    added .dot version as well for the normal equation

--- ex1/normalEqn.py
+++ ex1/normalEqn.py
@@ -15,3 +15,8 @@ def normalEqn(X,y):
 
 # ============================================================
 
+"""
+def normalEqn(X, y):
+    theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+    return theta
+"""
